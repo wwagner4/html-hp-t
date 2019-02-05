948f7f2909f44c699d57ad88bcf8c59f1e2da5be
wwagner4 <wwagner4@gmail.com>
2019-02-05 14:38:50 +0100

    some formatting

--- src/main/scala/net/entelijan/tf/Generator.scala
+++ src/main/scala/net/entelijan/tf/Generator.scala
@@ -122,70 +122,70 @@ object T {
 
   def htmlContetntRight(p: Page): String =
     s"""
-<div id="right">
-    <div class="flexslider">
-    <ul class="slides">
-${htmlImageList(p)}
-    </ul>
-    </div>
-</div>
-"""
+       |<div id="right">
+       |    <div class="flexslider">
+       |    <ul class="slides">
+       |${htmlImageList(p)}
+       |    </ul>
+       |    </div>
+       |</div>
+       |""".stripMargin
 
   def htmlTemplate(p: Page): String =
     s"""
-<!DOCTYPE html>
-<html class="no-js">
-<head>
-<title>das taschenfahrrad</title>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<link href='taschenfahrrad.css'	rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="flexslider.css" type="text/css" media="screen" />
-<script src="js/modernizr.js"></script>
-<style type="text/css">
-</style>
-</head>
-<body class="load">
-<div id="all">
-${p.htmlContent}
-</div>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\\x3C/script>')</script>
-<script defer src="jquery.flexslider.js"></script>
-<script type="text/javascript">
-$$(window).load(function() {
-	  $$('.flexslider').flexslider({
-	    start: function(slider) { // Fires when the slider loads the first slide
-	      var slide_count = slider.count - 1;
-	      $$(slider)
-	        .find('img.lazy:eq(0)')
-	        .each(function() {
-	          var src = $$(this).attr('data-src');
-	          $$(this).attr('src', src).removeAttr('data-src');
-	        });
-	    },
-	    before: function(slider) { // Fires asynchronously with each slider animation
-	      var slides     = slider.slides,
-	          index      = slider.animatingTo,
-	          $$slide     = $$(slides[index]),
-	          $$img       = $$slide.find('img[data-src]'),
-	          current    = index,
-	          nxt_slide  = current + 1,
-	          prev_slide = current - 1;
-
-	      $$slide
-	        .parent()
-	        .find('img.lazy:eq(' + current + '), img.lazy:eq(' + prev_slide + '), img.lazy:eq(' + nxt_slide + ')')
-	        .each(function() {
-	          var src = $$(this).attr('data-src');
-	          $$(this).attr('src', src).removeAttr('data-src');
-	        });
-	    }
-	  });
-	});
-</script>
-</body>
-</html>
-    """
+       |<!DOCTYPE html>
+       |<html class="no-js">
+       |<head>
+       |<title>das taschenfahrrad</title>
+       |<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+       |<link href='taschenfahrrad.css'	rel='stylesheet' type='text/css'>
+       |<link rel="stylesheet" href="flexslider.css" type="text/css" media="screen" />
+       |<script src="js/modernizr.js"></script>
+       |<style type="text/css">
+       |</style>
+       |</head>
+       |<body class="load">
+       |<div id="all">
+       |${p.htmlContent}
+       |</div>
+       |<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+       |<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\\x3C/script>')</script>
+       |<script defer src="jquery.flexslider.js"></script>
+       |<script type="text/javascript">
+       |$$(window).load(function() {
+       |	  $$('.flexslider').flexslider({
+       |	    start: function(slider) { // Fires when the slider loads the first slide
+       |	      var slide_count = slider.count - 1;
+       |	      $$(slider)
+       |	        .find('img.lazy:eq(0)')
+       |	        .each(function() {
+       |	          var src = $$(this).attr('data-src');
+       |	          $$(this).attr('src', src).removeAttr('data-src');
+       |	        });
+       |	    },
+       |	    before: function(slider) { // Fires asynchronously with each slider animation
+       |	      var slides     = slider.slides,
+       |	          index      = slider.animatingTo,
+       |	          $$slide     = $$(slides[index]),
+       |	          $$img       = $$slide.find('img[data-src]'),
+       |	          current    = index,
+       |	          nxt_slide  = current + 1,
+       |	          prev_slide = current - 1;
+       |
+|	      $$slide
+       |	        .parent()
+       |	        .find('img.lazy:eq(' + current + '), img.lazy:eq(' + prev_slide + '), img.lazy:eq(' + nxt_slide + ')')
+       |	        .each(function() {
+       |	          var src = $$(this).attr('data-src');
+       |	          $$(this).attr('src', src).removeAttr('data-src');
+       |	        });
+       |	    }
+       |	  });
+       |	});
+       |</script>
+       |</body>
+       |</html>
+       |""".stripMargin
 
   def fileName(p: Page): String = "%s.html" format p.id
 
