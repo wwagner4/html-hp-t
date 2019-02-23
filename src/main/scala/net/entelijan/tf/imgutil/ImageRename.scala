2d24164a37b4df0ce4ff2a85594949bf873f5161
wwagner4 <wwagner4@gmail.com>
2019-02-23 17:31:47 +0100

    neue bilder

---
+++ src/main/scala/net/entelijan/tf/imgutil/ImageRename.scala
@@ -0,0 +1,27 @@
+package net.entelijan.tf.imgutil
+
+import java.nio.file.{Files, Path, Paths}
+import collection.JavaConverters._
+
+
+object ImageRename extends App {
+
+  val dir = Paths.get("/home/wwagner4/tmp")
+
+  Files.list(dir)
+    .iterator()
+    .asScala.toList
+    .filter(p => !p.getFileName.toString.startsWith("."))
+    .sortBy(p => p.getFileName.toString)
+    .zipWithIndex
+    .map{case (p, i) => Files.copy(p, newPath(p, i))}
+
+
+  def newPath(oldPath: Path, index: Int): Path = {
+    val p = oldPath.getParent
+    val out = p.resolve("out")
+    Files.createDirectories(out)
+    out.resolve(s"tf_0223_$index.jpg")
+  }
+
+}
