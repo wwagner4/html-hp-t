a3ffeff605e8801e168c96cd0afcbba672b82050
wwagner4 <wwagner4@gmail.com>
2019-02-26 20:21:25 +0100

    Tiles from snapshot version

--- src/main/scala/net/entelijan/tf/tiles/TilesFromDirectory.scala
+++ src/main/scala/net/entelijan/tf/tiles/TilesFromDirectory.scala
@@ -31,7 +31,7 @@ object TilesFromDirectory {
 
     Thumbnails.of(files:_*)
       .scale(0.3)
-      .rotate(10.2)
+      .outputQuality(1.0)
       .toFiles(destDir.toFile, Rename.NO_CHANGE)
   }
 
