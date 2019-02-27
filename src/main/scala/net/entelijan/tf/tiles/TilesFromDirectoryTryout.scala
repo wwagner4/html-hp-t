4a8c493e6c0dbfc0f8ef95a5bb25a709510bd211
wwagner4 <wwagner4@gmail.com>
2019-02-27 08:19:46 +0100

    pfad für mac

--- src/main/scala/net/entelijan/tf/tiles/TilesFromDirectoryTryout.scala
+++ src/main/scala/net/entelijan/tf/tiles/TilesFromDirectoryTryout.scala
@@ -4,7 +4,8 @@ import java.nio.file.Paths
 
 object TilesFromDirectoryTryout extends App {
 
-   val prjDir = Paths.get("/home/wwagner4/prj/taschenfahrrad-2019/html-hp-t")
+   val prjDir = Paths.get("/Users/wwagner4/prj/html-hp-t")
+  //val prjDir = Paths.get("/home/wwagner4/prj/taschenfahrrad-2019/html-hp-t")
   //val prjDir = Paths.get("C:/ta30/entw1/html-hp-t")
   val indir = prjDir.resolve("src/main/web/images/index")
   println(s"indir:$indir")
