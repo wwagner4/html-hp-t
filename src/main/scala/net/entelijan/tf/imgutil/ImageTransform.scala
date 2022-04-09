e2550a0d0ce039d72b37c17ec3216ec383afea1e
wwagner4 <wwagner4@gmail.com>
2022-04-09 19:35:23 +0200

    shrink too large images

---
+++ src/main/scala/net/entelijan/tf/imgutil/ImageTransform.scala
@@ -0,0 +1,60 @@
+package net.entelijan.tf.imgutil
+
+import java.nio.file.{Files, Path}
+import scala.sys.process._
+
+
+object ImageTransform {
+
+  private val size = 400
+  private val shrinkSize = 1500
+  private val quality = 80
+
+  def createAllThumbnails(imgDir: Path): Unit = {
+    createAll(imgDir, createThumbnail)
+  }
+
+  def shrinkAll(imgDir: Path): Unit = {
+    createAll(imgDir, shrinkImage)
+  }
+
+  private def createThumbnail(img: Path): Unit = {
+    val outDir = img.getParent.resolve("tn")
+    if (Files.notExists(outDir)) Files.createDirectories(outDir)
+    if (Files.isRegularFile(img)) {
+      val nam = img.getFileName
+      val tnPath = outDir.resolve(nam)
+      if (Files.notExists(tnPath)) {
+        val cmd = s"convert ${img.toAbsolutePath} -thumbnail ${size}x$size^ -quality $quality% -gravity center -extent ${size}x$size ${tnPath.toAbsolutePath}"
+        println(s"creating thumbnail running imagemagick: '$cmd'")
+        cmd.!!
+      }
+    }
+  }
+
+  private def shrinkImage(img: Path): Unit = {
+    if (Files.isRegularFile(img) && Files.size(img) > 5.0e6) {
+      println(s"Shrinking image $img size: ${Files.size(img)}")
+      val cmd = s"mogrify -resize $shrinkSize> ${img.toAbsolutePath}"
+      println(s"shrinking image running imagemagick: '$cmd'")
+      cmd.!!
+    }
+  }
+
+  private def createAll(imgDir: Path, f: Path => Unit): Unit = {
+
+    if (Files.notExists(imgDir)) throw new IllegalStateException(s"Image directory ${imgDir.toAbsolutePath} does not exist")
+
+    def createThumbnails(dir: Path): Unit = {
+
+      if (Files.isDirectory(dir)) {
+        println(s"Creating thumbnails for $dir")
+        val tnDir = dir.resolve("tn")
+        if (Files.notExists(tnDir)) Files.createDirectory(tnDir)
+        if (Files.isDirectory(dir)) Files.list(dir).forEach(img => f(img))
+      }
+    }
+    println(s"Image directory ${imgDir.toAbsolutePath}")
+    Files.list(imgDir).forEach(dir => createThumbnails(dir))
+  }
+}
