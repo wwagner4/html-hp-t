c0bcd7e4d5707fe7e54dfd8be96219a4e33b7d71
wwagner4 <atnebel2323>
2023-07-30 22:40:53 +0200

    added Sommerpause

--- src/main/scala/net/entelijan/tf/Data.scala
+++ src/main/scala/net/entelijan/tf/Data.scala
@@ -28,6 +28,8 @@ object Data {
          |<p>tourenräder und fahrradtaschen<p>
          |
          |<p class="sepa4" />
+         |<p class="p2">Sommerpause 1. bis 31. August </p>
+         |<p class="sepa3" />
          |<p class="p2">Öffnungszeiten di-fr 15-18 sa 9-15</p>
          |<p>Individuelle Beratung für Eigenbauräder nach Vereinbarung jeweils di-fr 18-19</p>
          |<p><a target="_blank" href="http://www.openstreetmap.org/?lat=48.218173500000006&amp;lon=16.377131&amp;zoom=17&amp;layers=M&amp;mlat=48.21819&amp;mlon=16.37711">Leopoldsgasse 28 1020 Wien...</a></p>
