d45d21258aee363c8caea86cf326123fb45ac2b1
wwagner4 <wwagner4@gmail.com>
2026-01-05 20:51:51 +0100

    cli

--- src/main/scala/net/entelijan/tf/Main.scala
+++ src/main/scala/net/entelijan/tf/Main.scala
@@ -1,28 +1,45 @@
 package net.entelijan.tf
 
-trait Command
+sealed trait Command
 object Generate extends Command
 object Images extends Command
 case class Error(message: String) extends Command
+case class Help(message: String) extends Command
 
 object Main {
 
   def parseCmd(args: Array[String]): Command = {
+    val hlpMessage = """
+    |Run one of the following commands:
+    | - generate: Generates the homepage
+    | - images: Creates an image overview
+    """.stripMargin
+
     if args.size == 0 then
       Error("No command give. use one of 'generate', 'images'")
-    else Error("Not yet implemanted")
+    else {
+      val x = args(0).toLowerCase().trim()
+      println(f"--- $x")
+      if x.startsWith("gen") then Generate
+      else if x.startsWith("ima") then Images
+      else if x.startsWith("help") then Help(hlpMessage)
+      else if x.startsWith("-h") then Help(hlpMessage)
+      else if x.startsWith("--he") then Help(hlpMessage)
+      else
+        Error(f"Illegal command '${args(0)}'. use one of 'generate', 'images'")
+    }
   }
 
   def main(args: Array[String]): Unit = {
 
     parseCmd(args) match {
-      case Generate   => println("## generate")
-      case Images     => println("## images")
-      case Error(msg) => println(f"## error: $msg")
+      case Generate =>
+        val templ = Templs.tiles
+        Generator.gen(TfUtil.inTargetDir(s"gen-${templ.id}"), templ)
+      case Images     => ???
+      case Help(msg)  => println(msg)
+      case Error(msg) => println(f"Error: $msg")
     }
-
-    // val templ = Templs.tiles
-    // Generator.gen(TfUtil.inTargetDir(s"gen-${templ.id}"), templ)
 
   }
 }
