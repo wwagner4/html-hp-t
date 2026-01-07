1ade76ab0e3a2dfab5bc771cd2515a7eedc6a544
wwagner4 <wwagner4@gmail.com>
2026-01-07 21:38:28 +0100

    overview

--- src/main/scala/net/entelijan/tf/Main.scala
+++ src/main/scala/net/entelijan/tf/Main.scala
@@ -6,15 +6,20 @@ import mainargs.{main, arg, Parser, Flag}
 // Docu mainargs can be found in https://github.com/com-lihaoyi/mainargs
 object Main {
 
-  @main(name = "gen-homepage", doc = "Generates the homepage")
+  @main(name = "generate", doc = "Generates the homepage")
   def generateHomepage() = {
     val templ = Templs.tiles
     Generator.gen(TfUtil.inTargetDir(s"gen-${templ.id}"), templ)
   }
 
-  @main(name = "prep-images", doc = "Prepares new images to be integrated")
+  @main(name = "prepare", doc = "Prepares new images to be integrated")
   def prepareImages() = {
     ImgPrepare.main()
+  }
+
+  @main(name = "overview", doc = "Creates an overview of the current images")
+  def overviewImages() = {
+    ImgOverview.main()
   }
 
   def main(args: Array[String]): Unit =
--- src/main/scala/net/entelijan/tf/imgutil/ImgOverview.scala
+++ src/main/scala/net/entelijan/tf/imgutil/ImgOverview.scala
@@ -1,27 +1,61 @@
 package net.entelijan.tf.imgutil
 
+import net.entelijan.tf.{Data, ResCopy}
 import java.io.PrintWriter
 import java.nio.file.{Files, Path, Paths}
 
 import scala.jdk.CollectionConverters._
 import scala.util.control.NonFatal
 
-case class Img(path: String, nr: Int, imgName: String)
+enum Origin:
+  case New
+  case Old
+
+case class Img(path: String, origin: Origin, nr: Int, imgName: String)
 
 object ImgOverview {
 
   def main(): Unit = {
-    Seq("index", "jobs", "selfmade", "service", "0223").foreach(n => page(n))
+    Data.pages.foreach(n => page(n.id))
+  }
+
+  def betterName(name: String): String = {
+    return if name == "selfmade" then "surly"
+    else name
   }
 
   def page(name: String): Unit = {
-    val outPath = Paths.get("target/gen", s"Bilder_$name.html")
-    val inPath = Paths.get(s"src/main/web/images/$name")
+    val outDir =
+      Path.of(System.getProperty("user.home"), "tmp", "tf", "overview")
+    val ncol = 10
+
+    val inPath = Paths.get(s"src/main/web/common/images/$name")
+    if Files.notExists(inPath) then
+      throw IllegalStateException(s"$inPath does not exist")
+    if (Files.notExists(outDir)) Files.createDirectory(outDir)
+    val outPath = outDir.resolve(s"Bilder_${betterName(name)}.html")
+    ResCopy.copyDir(inPath, outDir)
 
     def toImg(index: Int, path: Path): Img = {
       val fname = path.getFileName.toString
-      val spath = s"images/$name/$fname"
-      Img(spath, index + 1, fname)
+      val splited = fname.split("-")
+      val origin = splited(1) match {
+        case "0" => Origin.New
+        case "1" => Origin.Old
+        case any =>
+          throw IllegalStateException(
+            s"Unknown origin '$any'. Must be '0' or '1"
+          )
+      }
+      val nr = splited(2).split("\\.")(0).toInt
+
+      val spath = s"$name/$fname"
+      Img(
+        path = spath,
+        nr = nr,
+        imgName = fname,
+        origin = origin
+      )
     }
 
     def createRows(inPath: Path): String = {
@@ -34,25 +68,37 @@ object ImgOverview {
         .sortBy(p => p.getFileName.toString)
         .zipWithIndex
         .map { case (path, idx) => toImg(idx, path) }
-        .grouped(4)
+        .grouped(ncol)
         .map(imgs => row(imgs))
         .mkString("\n")
     }
 
     withResources(new PrintWriter(Files.newBufferedWriter(outPath))) { pw =>
+      val css = """
+      |body {font-family: sans; }
+      |tr { margin-top: 3em; }
+      |.imov_txt { vertical-align: bottom; padding-top: 3em}
+      |.imov_title { font-size: 2em; }
+      """
+
       val rows = createRows(inPath)
       pw.print(s"""
              |<html>
              |<head>
+             |<style>
+             |$css
+             |</style>
              |</head>
              |<body>
-             |<p class="imov_title">$name</p>
+             |<p class="imov_title">${betterName(name)}</p>
              |<table class="imov_table">
              |$rows
              |</table>
              |</body>
              |</html>
         """.stripMargin)
+
+      println(s"Wrote overview to $outPath")
     }
 
     def row(imgs: Seq[Img]): String = {
@@ -65,9 +111,12 @@ object ImgOverview {
     }
 
     def createTd(img: Img): String = {
+      val marker = if img.nr < 16 then "#" else ""
       s"""
-         |<td><img class="imov_img" src="${img.path}">
-         |<p class="imov_txt">${img.nr} -- ${img.imgName}</p><td>
+         |<td class="imov_txt">
+         |<img class="imov_img" src="${img.path}" width="150"></br>
+         |${img.origin} ${img.nr} $marker
+         |</td>
        """.stripMargin
     }
 
