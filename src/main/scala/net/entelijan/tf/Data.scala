bbc5c023014eeea81c4840f249e9c02e52c3b2fd
wwagner4 <wwagner4@gmail.com>
2026-04-14 07:13:19 +0200

    Shurly änderungen mail hans

--- src/main/scala/net/entelijan/tf/Data.scala
+++ src/main/scala/net/entelijan/tf/Data.scala
@@ -75,20 +75,19 @@ object Data {
           |<p>alle Größen & Farben</p>
           |
           |<p>
-          |<a target="_blank" href="https://surlybikes.com/bikes/disc_trucker" >DiscTrucker 799€..</a><br>
+          |<a target="_blank" href="https://surlybikes.com/bikes/disc_trucker" >DiscTrucker 899€..</a><br>
           |<a target="_blank" href="https://surlybikes.com/bikes/karate_monkey" >KarateMonkey 999€..</a><br>
           |<a target="_blank" href="https://surlybikes.com/bikes/krampus" >Krampus 999€..</a><br>
           |<a target="_blank" href="https://surlybikes.com/bikes/ogre" >Ogre 899€..</a><br>
           |<a target="_blank" href="https://surlybikes.com/collections/bridge-club" >BridgeClub 749€..</a><br>
           |<a target="_blank" href="https://surlybikes.com/bikes/preamble" >Preamble 599€..</a><br>
-          |<a target="_blank" href="https://surlybikes.com/collections/straggler" >Straggler 799€ preis gilt bis 1.3.2026..</a><br>
+          |<a target="_blank" href="https://surlybikes.com/collections/new-straggler" >Straggler 2.0 949€...</a><br>
+          |<a target="_blank" href="https://surlybikes.com/collections/midnight-special" >Midnight Special 949€...</a><br>
           |</p>
           |
           |<p>
           |Surly legacy framesets, Größen und Farben auf Anfrage<br>
-          |<a target="_blank" href="https://surlybikes.com/products/midnight_special_2018" >Midnight special 899€</a><br>
-          |Straggler 2.0 899€<br>
-          |Legacy Straggler 699€<br>
+          |<a target="_blank" href="https://surlybikes.com/collections/straggler" >Straggler 699€...</a><br>
           |<a target="_blank" href="https://surlybikes.com/bikes/legacy/cross_check_2000" >CrossCeck 629€..</a><br>
           |PackRat 599€<br>
           |LongHaulTrucker 579€<br>
@@ -100,7 +99,7 @@ object Data {
           |<a target="_blank" href="https://pelagobicycles.com/airisto-commuter/" >Pelago Airisto 549€..</a><br>
           |<a target="_blank" href="https://www.brothercycles.com/shop/frames/mr-wooden/" >Brother Cycles Mr. Wooden 799€..</a><br>
           |Pelago Capri & San Sebastian<br>
-          |<a target="_blank" href="https://paripa.de/?page_id=390" >Paripa JWD, Paripa Kserie, Paripa meral in..</a><br>
+          |<a target="_blank" href="https://paripa.de/?page_id=390" >Paripa JWD, Paripa Kserie, Paripa meral...</a><br>
           |Restgrößen & im Abverkauf 399€<br>
           |</p>
           |""".stripMargin
